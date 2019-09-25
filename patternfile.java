b47604d533417729eae1347773b75a13615da232
root <root@ip-172-31-21-218.us-west-2.compute.internal>
2019-09-25 07:21:05 +0000

    adding patter file

---
+++ patternfile.java
@@ -0,0 +1,25 @@
+ public class pattern  
+{    
+     public static void main(String[] args){    
+    int lines=4;    
+    int i,j;    
+    for(i=1;i<=lines;i++){// this loop is used to print lines    
+      for(j=1;j<=lines;j++){// this loop is used to print * in a line    
+          if(i==j)    
+             System.out.print("*");    
+            else    
+           System.out.print("0");    
+      }    
+      j--;    
+       System.out.print("*");    
+      while(j>=1){// this loop is used to print * in a line    
+          if(i==j)    
+           System.out.print("*");    
+          else    
+           System.out.print("0");    
+          j--;    
+      }    
+    System.out.println("");    
+  }    
+         }    
+}
